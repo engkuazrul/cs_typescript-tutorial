d6a56cdd285187a61fa8f0b9cc2f960f424b4b34
engkuazrul <engku@dormeo.my>
2022-01-21 09:09:31 +0800

    create basic style for app, and create input field component

---
+++ src/components/InputField.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.css';
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.FC<Props> = ({todo, setTodo}) => {
+    return (
+        <form className='input'>
+            <input type='text' placeholder='Enter a task' className='input__box' />
+            <button className='input_submit' type='submit'>Go</button>
+        </form>
+    );
+};
+
+export default InputField;

6941f923c7965566d1c4bdb95dccf768fab54db6
engkuazrul <engku@dormeo.my>
2022-01-21 09:39:56 +0800

    update app js

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -4,12 +4,13 @@ import './styles.css';
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField: React.FC<Props> = ({todo, setTodo}) => {
+const InputField: React.FC<Props> = ({todo, setTodo, handleAdd}) => {
     return (
-        <form className='input'>
-            <input type='text' placeholder='Enter a task' className='input__box' />
+        <form className='input' onSubmit={handleAdd}>
+            <input type='text' placeholder='Enter a task' className='input__box' value={todo} onChange={(e) => setTodo(e.target.value)}/>
             <button className='input_submit' type='submit'>Go</button>
         </form>
     );
